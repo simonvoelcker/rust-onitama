c0821b7ccdcc3a22e28f89cf6cb94c91f4aedaa7
simon <epic.fail6174@googlemail.com>
2020-09-02 10:15:14 +0200

    Implemented all cards

--- src/card.rs
+++ src/card.rs
@@ -6,6 +6,77 @@ pub struct Card {
 	pub moves: Vec<Offset>
 }
 
+impl Card {
+	pub fn get_all_cards() -> Vec<Card> {
+		return vec![
+			Card {
+				name: "Affe".to_string(),
+				moves: vec![Offset {x:-1,y:1}, Offset {x:1,y:1}, Offset {x:-1,y:-1}, Offset {x:1,y:-1}]
+			},
+			Card {
+				name: "Drache".to_string(),
+				moves: vec![Offset {x:-2,y:1}, Offset {x:2,y:1}, Offset {x:-1,y:-1}, Offset {x:1,y:-1}]
+			},
+			Card {
+				name: "Elefant".to_string(),
+				moves: vec![Offset {x:-1,y:1}, Offset {x:1,y:1}, Offset {x:-1,y:0}, Offset {x:1,y:0}]
+			},
+			Card {
+				name: "Krabbe".to_string(),
+				moves: vec![Offset {x:0,y:1}, Offset {x:-2,y:0}, Offset {x:2,y:0}]
+			},
+			Card {
+				name: "Tiger".to_string(),
+				moves: vec![Offset {x:0,y:2}, Offset {x:0,y:-1}]
+			},
+			Card {
+				name: "Gans".to_string(),
+				moves: vec![Offset {x:-1,y:1}, Offset {x:-1,y:0}, Offset {x:1,y:0}, Offset {x:1,y:-1}]
+			},
+			Card {
+				name: "Hahn".to_string(),
+				moves: vec![Offset {x:1,y:1}, Offset {x:-1,y:0}, Offset {x:1,y:0}, Offset {x:-1,y:-1}]
+			},
+			Card {
+				name: "Ochse".to_string(),
+				moves: vec![Offset {x:0,y:1}, Offset {x:1,y:0}, Offset {x:0,y:-1}]
+			},
+			Card {
+				name: "Pferd".to_string(),
+				moves: vec![Offset {x:0,y:1}, Offset {x:-1,y:0}, Offset {x:0,y:-1}]
+			},
+			Card {
+				name: "Wildschwein".to_string(),
+				moves: vec![Offset {x:0,y:1}, Offset {x:-1,y:0}, Offset {x:1,y:0}]
+			},
+			Card {
+				name: "Aal".to_string(),
+				moves: vec![Offset {x:-1,y:1}, Offset {x:1,y:0}, Offset {x:-1,y:-1}]
+			},
+			Card {
+				name: "Gottesanbeterin".to_string(),
+				moves: vec![Offset {x:-1,y:1}, Offset {x:1,y:1}, Offset {x:0,y:-1}]
+			},
+			Card {
+				name: "Kobra".to_string(),
+				moves: vec![Offset {x:1,y:1}, Offset {x:-1,y:0}, Offset {x:1,y:-1}]
+			},
+			Card {
+				name: "Kranich".to_string(),
+				moves: vec![Offset {x:0,y:1}, Offset {x:-1,y:-1}, Offset {x:1,y:-1}]
+			},
+			Card {
+				name: "Frosch".to_string(),
+				moves: vec![Offset {x:-1,y:1}, Offset {x:-2,y:0}, Offset {x:1,y:-1}]
+			},
+			Card {
+				name: "Hase".to_string(),
+				moves: vec![Offset {x:1,y:1}, Offset {x:2,y:0}, Offset {x:-1,y:-1}]
+			},
+		];
+	}
+}
+
 impl fmt::Display for Card {
 	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
 		write!(f, "{}", self.name)
--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,13 @@ mod position;
 mod card;
 
 use field::Field;
-use position::Offset;
 use card::Card;
 
 fn main() {
 	let field = Field::new();
     println!("{}", field);
 
-    let cards: Vec<Card> = vec![
-    	Card {
-    		name: "Monkey".to_string(),
-    		moves: vec![Offset {x:-1,y:-1}, Offset {x:1,y:-1}, Offset {x:-1,y:1}, Offset {x:1,y:1}]
-    	}
-    ];
+    let cards = Card::get_all_cards();
 
     let all_pieces = &field.get_all_pieces(true);
     for (piece, position) in all_pieces.iter() {
