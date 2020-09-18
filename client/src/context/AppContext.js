718deace4a1cc16c5bda353d8e2a5732ce4ccd24
simon <epic.fail6174@googlemail.com>
2020-09-18 12:34:40 +0200

    Minor stuff

--- client/src/context/AppContext.js
+++ client/src/context/AppContext.js
@@ -2,11 +2,7 @@ import React, { Component } from 'react'
 
 import axios from 'axios'
 
-const $axios = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? '/api/' : 'http://localhost:3030/api/',
-  timeout: 10000,
-  headers: { 'Content-Type': 'application/json' }
-})
+const $axios = axios.create({ baseURL: '/api/', timeout: 10000, headers: { 'Content-Type': 'application/json' }})
 
 export const AppContext = React.createContext()
 
